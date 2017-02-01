562ab8d97df18a2b79f60f67913aa166e921d3d7
Niranjan <nbniranjan38@gmail.com>
2017-02-01 08:15:57 -0500

    recommendation logic added

--- Movie Anlaysis/movies.sbt
+++ Movie Anlaysis/movies.sbt
@@ -7,4 +7,6 @@ scalaVersion :="2.11.8"
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "1.6.0" % "provided",
   "org.apache.spark" %% "spark-sql" % "1.6.0",
+  "org.apache.spark" %% "spark-hive" % "1.6.0",
+  "org.apache.spark" %% "spark-mllib" % "1.6.0",
   "com.databricks" %"spark-csv_2.11"% "1.5.0")
--- Movie Anlaysis/movies.scala
+++ Movie Anlaysis/movies.scala
@@ -1,21 +1,57 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.hive.HiveContext
+import org.apache.spark.mllib.linalg._
+import org.apache.spark.mllib.recommendation.{ALS,MatrixFactorizationModel,Rating}
+
 
 object movies{
 
 def main(args:Array[String] ){
 //initialize
 val sc = new SparkContext()
-val sqlContext = new SQLContext(sc)
+val sqlContext = new HiveContext(sc)
 
 //data setup
-val ratingsDF = sqlContext.read.format("com.databricks.spark.csv").option("inferschema","true").option("header","true").load("C:\\cs518_hadoop\\moviesmall\\ml-latest-small\\ratings.csv").registerTempTable("ratings")
-val moviesDF = sqlContext.read.format("com.databricks.spark.csv").option("inferschema","true").option("header","true").load("C:\\cs518_hadoop\\moviesmall\\ml-latest-small\\movies.csv").registerTempTable("movies")
+val ratingsDF = sqlContext.read.format("com.databricks.spark.csv").option("inferschema","true").option("header","true").load("input/ratings.csv")
+val moviesDF = sqlContext.read.format("com.databricks.spark.csv").option("inferschema","true").option("header","true").load("input/movies.csv")
+
+ratingsDF.registerTempTable("ratings")
+moviesDF.registerTempTable("movies")
 
 
-val x= sqlContext.sql("select * from movies")
+val ratingsRDD = ratingsDF.rdd
+val moviesRDD = moviesDF.rdd
 
-x.take(10).foreach(println)
+val x= sqlContext.sql("select title,avg(rating) as rating,count(0) as cnt from movies,ratings where movies.movieId = ratings.movieId group by title order by rating desc,cnt desc")
+
+//x.write.save("output/movies.parquet")
+//x.show(10)
+
+//machine learning
+
+val rating = ratingsRDD.map{ x => Rating(x.getInt(0),x.getInt(1),x.getDouble(2))}
+
+rating.take(4).foreach(println)
+
+val ranking = 10
+val numIterations=10
+
+val model = ALS.train(rating, ranking, numIterations)
+
+val userPreferences=model.recommendProducts(1,5)
+
+import sqlContext.implicits._
+
+case class UserP(user:Int,movie:Int,Rating:Int)
+
+val userPDF = userPreferences.map{case Rating(user,product,rating) => UserP(user,product,rating)}
+
+
+
+sc.stop()
 
 }
 
